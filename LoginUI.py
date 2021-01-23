fc3976842c57bba1b8c1b786835aab9148b0a7e4
Ayushman Sengar <58720921+Ayushmansengar@users.noreply.github.com>
2021-01-23 19:30:10 +0530

    Update LoginUI.py

--- LoginUI.py
+++ LoginUI.py
@@ -2,32 +2,20 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
-from kivy.lang import Builder
-from kivy.uix.boxlayout import BoxLayout
 
-kv = """
-<LoginUI>:
-    orientation: "horizontal"
-    
 
-   
 
-   
-"""
-
-Builder.load_string(kv)
-
-class LoginUI(BoxLayout):
+class LoginUI(GridLayout):
 
     def __init__(self, **kwargs):
         super(LoginUI,self).__init__(**kwargs)
        
         self.cols = 2
-        self.add_widget(Label(text='user Name',height= 15))
-        self.username = TextInput(multiline=False,height= 15)
+        self.add_widget(Label(text='User Name',size_hint= (None, None),height= 15))
+        self.username = TextInput(multiline=False,size_hint= (None, None),height= 15)
         self.add_widget(self.username)
         self.add_widget(Label(text='Password',size_hint= (None, None),height= 15))
-        self.password = TextInput(password=True, multiline=False,size_hint= (.1, None),height= 15)
+        self.password = TextInput(password=True, multiline=False,size_hint= (None, None),height= 15)
         self.add_widget(self.password)
 
 class MyApp(App):
